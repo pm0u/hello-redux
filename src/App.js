b2eac8b703700105e61eab3018d6916c8b53394b
Paul Mourer <paul.mourer@gmail.com>
2019-01-20 10:31:17 -0700

    completed refactoring to use redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import HelloWorld from "./HelloWorld";
+import {createStore} from 'redux'
+import reducer from './reducers'
+
+const initialState = { tech: 'React ' }
+const store = createStore(reducer, initialState)
 
 class App extends Component {
-  state = {
-    tech: "React"
-  };
-  render() {
-    return <HelloWorld tech={this.state.tech} />;
-  }
+ // the state object has been removed.
+render() {
+  return <HelloWorld tech={store.getState().tech}/>
+}
 }
 
 export default App;
